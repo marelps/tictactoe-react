9f72c5b6b1038746e3a3eb29274b6c42cd437c88
marelps <vitoriagarrucho@gmail.com>
2022-04-10 18:01:07 -0300

    Adicionando 'viagem do tempo' ao jogo

--- src/index.jsx
+++ src/index.jsx
@@ -1,59 +1,41 @@
 // Importação do react, react-dom e a estrutura CSS do projeto
-import React from 'react'; 
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-function Square(props) { // Os quadrados são botões
+// Os quadrados do jogo da velha são botões nomeados como 'square'
+function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
-  
+
+// ========================================
 class Board extends React.Component {
+  /* Esse constructor deve ser deletado para o funcionamento do constructor na class Game, a class de mais alto nível, caso o projeto não tivesse o histórico de jogadas, esse constructor ainda estaria em funcionamento na class Board e não na Game
+
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
     };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-        return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-    });
-  }
-
+  }*/
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
-  
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-        status = 'Vencedor: ' + winner + '! Parabéns, ganhou um bolo'; // Caso tenha algum ganhador na partida, essa será a mensagem que aparecerá para o usuário
-    }
-    else {
-        status = 'Turno do : ' + (this.state.xIsNext ? 'X' : 'O'); // 'Caso contrário' ou seja, enquanto o jogo não terminar, será essa a mensagem que aparecerá, dando continuidade ao jogo
-    } // É possível fazer um novo else para caso tenha impate e não há um jogador, pois por enquanto a mensagem que irá aparecer é somente a que está agora, 'Turno do:' e o próximo jogador.
 
-    return ( // É aqui onde está renderizando os quadrados que é possível visualizar no projeto, todos eles numerados de 0 ao 8 e utilizando a estrutura do HTML
+  // É aqui onde está renderizando os quadrados que é possível visualizar no projeto, todos eles numerados de 0 ao 8 e utilizando a estrutura do HTML
+  render() {
+    return (
       <div>
-        <div className="status">{status}</div> 
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -73,33 +55,107 @@ class Board extends React.Component {
     );
   }
 }
+
+// ========================================
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      stepNumber: 0, // Definições de chave
+      xIsNext: true,
+    };
+  }
+  /* O state stepNumber que adicionamos reflete a jogada mostrada ao usuário nesse momento. Após fazermos uma nova jogada, precisamos atualizar stepNumber adicionando stepNumber: history.length como parte do argumento de this.setState. Isso certifica que não ficaremos presos mostrando a mesma jogada após uma novo ter sido feita. 
   
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+  O .slice em 'this.state.history.slice()' Certifica que se nós "voltarmos no tempo", e então fizermos uma nova jogada a partir daquele ponto, descartamos todo o histórido do "futuro" que agora se tornaria incorreto.
+  */
+  handleClick(i) {
+    const history = this.state.history.slice(0,
+        this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+    // Ao contrário do método de arrays push(), o método concat() não modifica o array original, por isso preferimos utilizá-lo.
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  /* Observe no método jumpTo, não atualizamos a propriedade de histórico do estado. Isso ocorre porque as atualizações de estado são mescladas ou, em palavras mais simples, o react atualizará apenas as propriedades mencionadas no método setState, deixando o estado restante como está. */
+  jumpTo(step) {
+      this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0,
+      })
   }
 
-  
-  
-  // ========================================
-  
-  ReactDOM.render( // Tem a função chamada render, onde é passado um parâmetro e executa um elemento
-    <Game />,
-    document.getElementById('root')
-  );
-  
-  function calculateWinner(squares) { // Função que determina o jogador, ou seja, aquele que preencha um dos três quadrados em uma das linhas, inclusive na vertical
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    // Array utilizado para mapear uma fonte de dados para outra fonte de dados
+    const moves = history.map((step, move) => { 
+        const desc = move ?
+        'Ir para o ' + move + 'º movimento' : 'Ir para o início do jogo'; // Texto que estará escrito no botão onde irá voltar determinadas jogadas da partida e também um outro botão para voltar ao início do jogo, como um reset
+        // Retornando o botão que conterá o histórico de jogos dito anteriormente no const desc
+        return ( // Definido as chaves da jogada
+            <li key={move}> 
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Vencedor: ' + winner + '! Parabéns, ganhou um bolo :D'; // Caso tenha algum ganhador na partida, essa será a mensagem que aparecerá para o usuário
+    } else {
+      status = 'Turno do : ' + (this.state.xIsNext ? 'X' : 'O'); // Caso contrário ou seja, enquanto o jogo não terminar, será essa a mensagem que aparecerá, dando continuidade ao jogo
+    }
+    // Obs: É possível fazer um novo else para caso tenha impate e não há um jogador, pois por enquanto a mensagem que irá aparecer é somente a que está agora, 'Turno do:' e o próximo jogador.
+
+    /* Aqui onde irá retornar todo o conteúdo do site.
+        Na div game-board é onde está os quadrados(button) do jogo junto com o efeito de clique que marca X e O, pois o valor padrão é null 
+        Na div game-info é onde mostra o status, aquele texto que escreve de quem deve ser a jogada atual e os botões listado numéricamentes <ol> ao lado com o histórico de movimentos e botão de resetar a partida do jogo.
+
+        /!\ Importante: Cada filho de um array ou iterator deve ter uma prop “key” única. /!\
+    */
+    return ( 
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+// Tem a função chamada render, onde é passado um parâmetro e executa um elemento
+ReactDOM.render( 
+  <Game />,
+  document.getElementById("root")
+);
+
+// Função que determina o jogador, ou seja, aquele que preencha um dos três quadrados em uma das linhas, inclusive na vertical
+function calculateWinner(squares) { 
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
